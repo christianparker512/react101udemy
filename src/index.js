5c3a34eed38e85ff04c52edd41f33b91aff66e15
Christian Parker <christianparker512@gmail.com>
2021-08-12 15:48:52 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+//hello world
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const name = 'Christian';
+
+
+function formatName(user){
+    return user.firstName + ' ' + user.lastName;
+}
+const user = {
+    firstName: 'Christian',
+    lastName: 'Parker'
+}
+const element = <h1>Hello {formatName(user)} from {name}</h1>
+ReactDOM.render(element,document.querySelector('#root') );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
