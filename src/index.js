d7dca722c00351b0b9c4c855d2b4088b45060e4c
Christian Parker <christianparker512@gmail.com>
2021-08-13 12:49:44 -0500

    tick refactored

--- src/index.js
+++ src/index.js
@@ -2,23 +2,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const users = [
-        {id: 1, name: 'christian', age: 50},
-        {id: 2, name: 'lisa', age: 56},
-        {id: 3, name: 'abby', age: 19},
-        {id: 4, name: 'quincy', age: 12},
-        {id: 5, name: 'baxter', age: 11}
-]
-function App(){
-        return (
-        <div>
-                {users.map(user =>(
-                    <Welcome key={user.id } name={user.name} age={user.age}/>
-                        ))};
-        </div>
-        );
+function Clock(){
+   return (
+       <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+       </div>
+    );
 }
-// const name = 'Christian';
+
+function tick(){
+        ReactDOM.render(<Clock />, document.getElementById('root'));
+}
+setInterval(tick,1000);
+// const users = [
+//         {id: 1, name: 'christian', age: 50},
+//         {id: 2, name: 'lisa', age: 56},
+//         {id: 3, name: 'abby', age: 19},
+//         {id: 4, name: 'quincy', age: 12},
+//         {id: 5, name: 'baxter', age: 11}
+// ]
+// function App(){
+//         return (
+//         <div>
+//                 {users.map(user =>(
+//                     <Welcome key={user.id } name={user.name} age={user.age}/>
+//                         ))};
+//         </div>
+//         );
+// }
+// // const name = 'Christian';
 //
 // const bigURL = 'https://rawpixel-live.imgix.net/collection/cover/360c882d84f757d213bc925d2fdd60d0_cover.jpg?bg=F4F4F3&con=3&cs=srgb&dpr=1&fit=clip&h=1200&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=1200&s=9de45879c5d9cfa9c27f8d913909081a'
 // const artist = 'Vincent Van Gogh'
@@ -39,13 +52,13 @@ function App(){
 //     ReactDOM.render(element, document.getElementById("root"));
 // },1000);
 
-function Welcome (props){
-        return (
-        <h1>Hello, {props.name}. You are {props.age}.
-        </h1>
-        );
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+// function Welcome (props){
+//         return (
+//         <h1>Hello, {props.name}. You are {props.age}.
+//         </h1>
+//         );
+// }
+// ReactDOM.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
