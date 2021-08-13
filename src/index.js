800899d001656133877e1f46fa8163ff4a6c6d20
Christian Parker <christianparker512@gmail.com>
2021-08-13 11:29:49 -0500

    set time using document manipulation with setting the interval

--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@ const name = 'Christian';
 const bigURL = 'https://rawpixel-live.imgix.net/collection/cover/360c882d84f757d213bc925d2fdd60d0_cover.jpg?bg=F4F4F3&con=3&cs=srgb&dpr=1&fit=clip&h=1200&ixlib=php-3.1.0&q=65&usm=15&vib=3&w=1200&s=9de45879c5d9cfa9c27f8d913909081a'
 const artist = 'Vincent Van Gogh'
 
-function formatName(user){
-    return user.firstName + ' ' + user.lastName;
-}
-const element = <div>
-    <img src = {bigURL} width="400px" alt = "Room" />
-    <p>{artist}</p>
-    </div>
+// function formatName(user){
+//     return user.firstName + ' ' + user.lastName;
+// }
+// const element = <div>
+//     <img src = {bigURL} width="400px" alt = "Room" />
+//     <p>{artist}</p>
+//     </div>
+setInterval(()=> {
+    const element = (
+        <div>
+            <h1>Hello, world</h1> <p>It's {new Date().toUTCString()}</p>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById("root"));
+},1000);
 
-ReactDOM.render(element, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
