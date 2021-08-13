d1f10eb23db8acc2430d573d373cc4891249a77c
Christian Parker <christianparker512@gmail.com>
2021-08-13 12:59:51 -0500

    set state using didmount and willunmount

--- src/index.js
+++ src/index.js
@@ -2,19 +2,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(){
-   return (
-       <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-       </div>
-    );
+class Clock extends React.Component {
+    constructor (props){
+        super(props)
+        this.state={date: new Date().toLocaleTimeString()}
+    }
+
+    componentDidMount(){
+        this.timerID = setInterval(() => this.tick(),1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+    tick(){
+        this.setState({
+            date: new Date().toLocaleTimeString()
+        })
+    }
+    render()
+    {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date}</h2>
+            </div>
+        );
+    }
 }
 
-function tick(){
-        ReactDOM.render(<Clock />, document.getElementById('root'));
-}
-setInterval(tick,1000);
+// function tick(){
+ReactDOM.render(<Clock />, document.getElementById('root'));
+
+// setInterval(tick,1000);
 // const users = [
 //         {id: 1, name: 'christian', age: 50},
 //         {id: 2, name: 'lisa', age: 56},
